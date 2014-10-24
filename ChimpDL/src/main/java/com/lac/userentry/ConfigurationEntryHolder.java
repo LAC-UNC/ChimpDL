54f56dc5b8ec23a5b2cd9070a24c446738356c08
Ricardo Fretes <rgfretes@gmail.com>
2014-10-24 20:47:52 -0300

    Removed some invalid characters for utf-8. Fixed dependency to Chimp

--- ChimpDL/src/main/java/com/lac/userentry/ConfigurationEntryHolder.java
+++ ChimpDL/src/main/java/com/lac/userentry/ConfigurationEntryHolder.java
@@ -34,10 +34,10 @@ public class ConfigurationEntryHolder extends Observable{
 
 	private static void createInstance() {
 		if (INSTANCE == null) {
-			// Sólo se accede a la zona sincronizada
-			// cuando la instancia no está creada
+			// Solo se accede a la zona sincronizada
+			// cuando la instancia no esta creada
 			synchronized(ConfigurationEntryHolder.class) {
-				// En la zona sincronizada sería necesario volver
+				// En la zona sincronizada seria necesario volver
 				// a comprobar que no se ha creado la instancia
 				if (INSTANCE == null) { 
 					INSTANCE = new ConfigurationEntryHolder();
